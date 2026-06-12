fe63a28079055b6113cb24a204a90cbd25b12a8e
xKaMeiKo <xkameiko@gmail.com>
2026-06-12 13:32:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ if not st.session_state.logged_in:
         email = st.text_input("E-mail")
         senha = st.text_input("Senha", type="password")
         if st.button("Entrar", use_container_width=True):
-            login_user(email, senate)
+            login_user(email, password)
     st.stop()
 
 # --- ÁREA LOGADA ---
